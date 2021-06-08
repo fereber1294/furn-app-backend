b87553e3b5509184d2876690b8931cb264c8793c
fereber1294 <freyesber@gmail.com>
2021-06-08 11:49:02 -0500

    backend done

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -29,18 +29,85 @@ exports.createItem = async (req,res) => {
 }
 
 exports.getItem = async (req,res) => {
-  console.log('hello');
   let user = req.user.id
-  console.log(user);
   try{
     const itemsList = await Items.find({creator:user}).sort({createdAt:-1})
-    console.log(itemsList);
     res.json({
-      projectsList
+      itemsList
     })
   }catch(e){
     res.status(400).json({
       msg:'Sorry, there was an error on the server side.'
+    })
+  }
+}
+
+exports.updateItem = async (req,res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.json({
+      errors: errors.array()
+    })
+  }
+  
+  //extract item info
+  const{name} = req.body
+  const newItem = {} //to host the new data
+  newItem.name = name
+
+  console.log(newItem);
+  itemId = req.params.id
+
+  try{
+    let item = await Items.findById(itemId)
+
+    //checks if the item exists by id
+    if(!item){
+      return res-status(400).json({
+        msg: "Item wasn't found"
+      })
+    }
+
+    //checks if the user is the creator of the item
+    if(item.creator.toString() !== req.user.id){
+      return res.status(400).json({
+        msg: "OOPS! You are trying to edit an item that wasn't created by you"
+      })
+    }
+
+    //update the project
+    project = await Items.findByIdAndUpdate({_id: itemId.toString()},{$set: newItem},{new:true})
+  }catch(e){
+    res.status(400).json({
+      msg: 'Sorry, there was an error on the server side.'
+    })
+  }
+}
+
+exports.deleteItem = async(req,res) => {
+  //get the data from the route
+  const itemId = req.params.id
+
+  //checks if the user is the creator of the item
+  if(item.creator.toString() !== req.user.id){
+    return res.status(400).json({
+      msg: "OOPS! You are trying to edit an item that wasn't created by you"
+    })
+  }
+
+  //Delete the item
+  await Items.findByIdAndRemove({_id: itemId})
+  res.json({
+    msg: "Item was removed from DB"
+  })
+
+  try{
+    let item = await Items.findById(itemId)
+
+
+  }catch(e){
+    res.status(400).json({
+      msg: 'Sorry, there was an error on the server side.'
     })
   }
 }
