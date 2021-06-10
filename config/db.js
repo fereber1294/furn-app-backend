1914183d178d7dad000190e35da1c9bf4a82eebf
fereber1294 <freyesber@gmail.com>
2021-06-10 12:04:14 -0500

    backend deploy test

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
   try{
-    await mongoose.connect('mongodb://localhost:27017/furnapp',{
+    await mongoose.connect(process.env.MONGODB_URL,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     })
     console.log('DB created');
   } catch(e){
-    console.log(e);
+    console.log(e)
     process.exit(1)
   }
 }
